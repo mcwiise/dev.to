26f9f4734131cdf14808409c4f53dd07500fba40
German Valencia Vargas <mcwiise@outlook.com>
2023-01-01 15:58:16 -0500

    lca and sorting bst

--- binary-tree-ds/src/btree/BinarySearchTree.java
+++ binary-tree-ds/src/btree/BinarySearchTree.java
@@ -6,15 +6,21 @@ public class BinarySearchTree extends BinaryTree{
     public static void main(String[] args) {
         var bts = new BinarySearchTree();
 
-        bts.root = bts.insert(bts.root, 9);
-        bts.insert(bts.root, 10);
-        bts.insert(bts.root, 11);
+        bts.root = bts.insert(bts.root, 8);
+        bts.insert(bts.root, 7);
+        bts.insert(bts.root, 9);
 
-        var nodeFound = bts.search(bts.root, 10);
+        var nodeFound = bts.search(bts.root, 7);
         var nodeNotFound = bts.search(bts.root, 15);
 
         System.out.println("---found------> " + nodeFound);
         System.out.println("---notFound---> " + nodeNotFound);
+        System.out.println("---ascending------> ");
+        bts.printAscending(bts.root);
+        System.out.println("\n---descending------> ");
+        bts.printDescending(bts.root);
+        var lca = bts.lca(bts.root, bts.search(bts.root, 7), bts.search(bts.root, 9));
+        System.out.println("\n---lca 7 9------> "+lca.value);
     }
 
     public Node search(Node parent, int key){
@@ -49,6 +55,36 @@ public class BinarySearchTree extends BinaryTree{
             var predecessor = this.inOrderPredecessor(parent);
             parent.value = predecessor.value;
             parent.left = this.delete(parent.left, key);
+        }
+        return parent;
+    }
+
+    public void printAscending(Node parent){
+        if (parent == null){
+            return;
+        } else {
+            this.printAscending(parent.left);
+            System.out.print(parent.value);
+            this.printAscending(parent.right);
+        }
+    }
+
+    public void printDescending(Node parent){
+        if (parent == null){
+            return;
+        } else {
+            this.printDescending(parent.right);
+            System.out.print(parent.value);
+            this.printDescending(parent.left);
+        }
+    }
+
+    public Node lca(Node parent, Node x, Node y) {
+        if(parent.value < x.value && parent.value < y.value) {
+            return lca(parent.right, x, y);
+        }
+        if(parent.value > x.value && parent.value > y.value) {
+            return lca(parent.left, x, y);
         }
         return parent;
     }
